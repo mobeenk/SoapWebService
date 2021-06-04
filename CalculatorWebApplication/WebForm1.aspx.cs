b33bbc8ba52c36e01809c2416ec84ef51f9ddcf8
Moubien Kayali <mobeen-k@hotmail.com>
2021-06-04 20:58:57 +0300

    Add project files.

---
+++ CalculatorWebApplication/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CalculatorWebApplication.CalculatorService;
+namespace CalculatorWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            // this is from the CalculatorService file Reference.cs
+            CalculatorWebServiceSoapClient client = new CalculatorWebServiceSoapClient();
+            int result =  client.AddTwoNumbers( 
+                Convert.ToInt32(txtFirstNumber.Text),
+                Convert.ToInt32(txtSecondNumber.Text)  
+               );
+            lblResult.Text = result.ToString();
+        }
+    }
+}
---
+++ SoapWebService/CalculatorWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapWebService
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://mywebsite.com/addnumbs")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : WebService
+    {
+
+        [WebMethod]
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            return num1+num2;
+        }
+    }
+}
